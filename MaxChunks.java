4f10738ebaf7a38f36dab92fdf791fdfc2a2e1ee
tamz2004 <ahamedtamzil123@gmail.com>
2024-12-19 22:02:23 +0530

    769. Max Chunks To Make Sorted

---
+++ MaxChunks.java
@@ -0,0 +1,13 @@
+
+class MaxChunks {
+    public int maxChunksToSorted(int[] arr) {
+        int ans = 0, mx = 0;
+        for (int i = 0; i < arr.length; ++i) {
+            mx = Math.max(mx, arr[i]);
+            if (i == mx) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+}
